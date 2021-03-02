607737d7be0c4e0a831d8fc16e31337a2661fc2f
theankushrai <iamankushrai@gmail.com>
2021-03-02 11:20:46 +0530

    1. poi click marker added. 2. displayed info when clicked in marker

--- app/src/main/java/com/example/wander/MapsActivity.kt
+++ app/src/main/java/com/example/wander/MapsActivity.kt
@@ -49,6 +49,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLong))
         //adding long click methord
         setMapLongClick(map)
+        //places of intrest like restraunts
+        setPoiClick(map)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -92,6 +94,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     .title(getString(R.string.dropped_pin))
                     .snippet(snippet)
             )
+        }
+    }
+
+    //setting places of intrest click
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                    MarkerOptions()
+                            .position(poi.latLng)
+                            .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
         }
     }
 }
